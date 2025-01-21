c95278b522138668eb315e592e6ccb0fefd5d002
Rajan2784 <rajansingh6616@gmail.com>
2025-01-21 21:33:47 +0530

    About Page created

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,10 +7,16 @@ const About = () => {
       <h1 className="text-center mt-10 text-2xl font-playwrite font-semibold text-green-500">
         About Us
       </h1>
-      <div>
-        <div>This is left section</div>
-        <div>
-          <p className="text-center mt-5 text-gray-600 text-lg md:text-xl">
+      <div className="grid grid-cols-8 mt-6 gap-3">
+        <div className="col-span-3">
+          <img
+            src="https://images.pexels.com/photos/3054690/pexels-photo-3054690.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=""
+            className="w-full object-cover object-center rounded-md"
+          />
+        </div>
+        <div className="col-span-5">
+          <p className=" mt-5 text-gray-600 text-lg md:text-xl">
             We are a team of foodies who love to cook and serve delicious meals
             to our customers. We are passionate about food and we believe that
             food is the best way to bring people together. Our mission is to
